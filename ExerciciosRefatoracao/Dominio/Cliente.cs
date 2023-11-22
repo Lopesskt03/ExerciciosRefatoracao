091eadbc246f1071d016ea8cdfcaebf3e8de714f
Lopesskt03 <luiz_lopesskt03@hotmail.com>
2023-11-22 19:44:13 -0300

    Initial commit

---
+++ ExerciciosRefatoracao/Dominio/Cliente.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosRefatoracao.Dominio
+{
+    public class Cliente
+    {
+        public RazaoSocial RazaoSocial { get; private set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public CNPJ CNPJ { get; private set; }
+
+        public Endereco EnderecoEntrega { get; private set; }
+        public Endereco EnderecoCobranca { get; private set; }
+        public Endereco EnderecoFaturamento { get; private set; }
+
+        public string GetTextoEndereco(Endereco endereco)
+        {
+            return $"Endereço: {endereco.Logradouro} {endereco.Numero} {endereco.Complemento} - {endereco.Bairro} - CEP {endereco.CEP} - {endereco.Municipio} - {endereco.UF}";
+        }
+
+        public string GetTextoEnderecoCobranca()
+        {
+            return GetTextoEndereco(EnderecoCobranca);
+        }
+
+        public string GetTextoEnderecoEntrega()
+        {
+            return GetTextoEndereco(EnderecoEntrega);
+        }
+
+        public string GetTextoEnderecoFaturamento()
+        {
+            return GetTextoEndereco(EnderecoFaturamento);
+        }
+
+        public void SetEnderecoFaturamento(string logradouro, string numero, string complemento, string bairro, string cep, string municipio, string uf)
+        {
+            EnderecoFaturamento = new Endereco(logradouro, numero, complemento, bairro, cep, municipio, uf);
+        }
+
+        public void SetEnderecoEntrega(string logradouro, string numero, string complemento, string bairro, string cep, string municipio, string uf)
+        {
+            EnderecoEntrega = new Endereco(logradouro, numero, complemento, bairro, cep, municipio, uf);
+        }
+
+        public void SetEnderecoCobranca(string logradouro, string numero, string complemento, string bairro, string cep, string municipio, string uf)
+        {
+            EnderecoCobranca = new Endereco(logradouro, numero, complemento, bairro, cep, municipio, uf);
+        }
+
+        public override string ToString()
+        {
+            return "===Dados do Cliente===\n" +
+                   $"CPF: {CPF}\n" +
+                   $"Nome: {Nome}\n" +
+                   $"{GetTextoEnderecoCobranca()}\n" +
+                   $"{GetTextoEnderecoEntrega()}\n" +
+                   $"{GetTextoEnderecoFaturamento()}";
+        }
+    }
+
+    public class Endereco
+    {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string CEP { get; private set; }
+        public string Municipio { get; private set; }
+        public string UF { get; private set; }
+
+        public Endereco(string logradouro, string numero, string complemento, string bairro, string cep, string municipio, string uf)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            CEP = cep;
+            Municipio = municipio;
+            UF = uf;
+        }
+    }
+
+    public class CNPJ
+    {
+        public string Codigo { get; private set; }
+
+        public CNPJ(string codigo)
+        {
+            Codigo = codigo;
+        }
+    }
+
+    public class RazaoSocial
+    {
+        public string Nome { get; private set; }
+
+        public RazaoSocial(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
---
+++ ExerciciosRefatoracao/Dominio/ClienteChat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ExerciciosRefatoracao.Dominio.Chat
+{
+    public class Cliente
+    {
+        public string RazaoSocial { get; private set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string CNPJ { get; set; }
+        public Endereco EnderecoEntrega { get; private set; }
+        public Endereco EnderecoCobranca { get; private set; }
+        public Endereco EnderecoFaturamento { get; private set; }
+
+        public Cliente(string cpf, string nome)
+        {
+            CPF = cpf;
+            Nome = nome;
+        }
+
+        public string GetTextoEndereco(Endereco endereco)
+        {
+            return $"Endereço: {endereco.Logradouro} {endereco.Numero} {endereco.Complemento} - {endereco.Bairro} - CEP {endereco.CEP} - {endereco.Municipio} - {endereco.UF}";
+        }
+
+        public void SetEnderecoFaturamento(Endereco endereco)
+        {
+            EnderecoFaturamento = endereco;
+        }
+
+        public void SetEnderecoEntrega(Endereco endereco)
+        {
+            EnderecoEntrega = endereco;
+        }
+
+        public void SetEnderecoCobranca(Endereco endereco)
+        {
+            EnderecoCobranca = endereco;
+        }
+
+        public override string ToString()
+        {
+            return "===Dados do Cliente===\n" +
+                   $"CPF: {CPF}\n" +
+                   $"Nome: {Nome}\n" +
+                   $"{GetTextoEndereco(EnderecoCobranca)}\n" +
+                   $"{GetTextoEndereco(EnderecoEntrega)}\n" +
+                   $"{GetTextoEndereco(EnderecoFaturamento)}";
+        }
+    }
+
+    public class Endereco
+    {
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Complemento { get; private set; }
+        public string Bairro { get; private set; }
+        public string CEP { get; private set; }
+        public string Municipio { get; private set; }
+        public string UF { get; private set; }
+
+        public Endereco(string logradouro, string numero, string complemento, string bairro, string cep, string municipio, string uf)
+        {
+            Logradouro = logradouro;
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = bairro;
+            CEP = cep;
+            Municipio = municipio;
+            UF = uf;
+        }
+    }       
+}
